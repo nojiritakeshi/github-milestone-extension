74ede779c9184ac12551bed8a03b1501e0f242e3
nojiritakeshi <takesoccer08nine@ezweb.ne.jp>
2024-04-04 19:49:17 +0900

    監視対象はmainの方が安定する

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { issueObserver } from './dateOver'
 import { registerMilestone } from './milestone'
 
 // data-turbo-bodyのDOMが変更されたとき発火
-const targetElement: Element = document.querySelector('body') || new Element()
+const targetElement: Element = document.querySelector('main') || new Element()
 const observer = new MutationObserver(() => {
   registerMilestone()
   void issueObserver()
